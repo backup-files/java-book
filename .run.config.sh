889333d4022e501209fd8ff4fde8a07373d98842
tejasmr <tejasmr@outlook.com>
2022-11-08 22:04:04 +0530

    Added run script and run script configuration

---
+++ .run.config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# make sure you don't end your run_script_folder with a /
+run_script_folder="./scripts"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# write your run related scripts'
 python run.py
